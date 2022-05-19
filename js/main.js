cecd6f330b14422fbcc10d616ea63258a9046ebb
rubacodes <rubacodes@gmail.com>
2022-05-19 15:25:57 +0200

    aggiunta immagine da data con bind

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 const app = new Vue({
   el: '#app',
   data: {
-    source: '',
+    source: 'http://via.placeholder.com/640x360',
     text: 'Testo random di prova',
+    alt: 'random image from the internet',
   },
 });
